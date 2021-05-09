0467022ff040701a97f20b417551d5d17e960f10
intom <iin.arahmah99@gmail.com>
2021-05-10 00:00:31 +0700

    file

---
+++ resources/views/pegawai/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout.main')
+
+@section('title', 'Pegawai')
+
+@section('container')
+      <div class="container">
+          <div class="row">
+              <div class="col">
+                  <h1 class="mt-3 mb-3">Data Pegawai</h1>
+
+                                  
+                  <table class="table">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">NIP</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Jabatan</th>
+                        <th scope="col">Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        
+                                             
+                      </tr>
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+
+              </div>
+          </div>
+      </div>
+@endsection
